f3dbb6cf5b0abe1e241b3862c0ca22ff5a926c01
katehuntsman <kate.e.huntsman@gmail.com>
2024-09-17 21:04:39 -0500

    adding folders and data

---
+++ sql/create_tables.sql
@@ -0,0 +1,22 @@
+-- Start by deleting any tables if the exist already
+-- We want to be able to re-run this script as needed.
+-- DROP tables in reverse order of creation 
+-- DROP dependent tables (with foreign keys) first
+
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE books (
+    book_id TEXT PRIMARY KEY,
+    title TEXT,
+    year_published INTEGER,
+    author_id TEXT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+CREATE TABLE authors (
+    author_id TEXT PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    year_born INTEGER
+);
